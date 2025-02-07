b45d1e4b63db1b96cc543be3c055deac3d44865c
Raphael <raphdesantos@gmail.com>
2025-02-07 14:52:21 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
 const port = 3000;
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
+app.use(express.json());
 app.get('/ping', (req, res) => {
   res.send('Ping successful!');
 });
